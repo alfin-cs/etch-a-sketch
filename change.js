4ee2cc540b665c137ea5cef3d806af69e6723954
alfin-cs <alfinsathesh@gmail.com>
2025-08-02 16:37:02 +0300

    delete the divs when new number is inputed

--- change.js
+++ change.js
@@ -1,11 +1,35 @@
 let main=document.querySelector(".main")
-for (i=0;i<100;i++){
-    let a=document.createElement("div")
-    a.style.flex=`1 1 ${100/10}%`
-    a.className="b"
-    main.appendChild(a)
-}
-main.addEventListener("hover",(event)=>{
-    let div=event.target
-    div.style.background
+
+let numSq=document.querySelector("#squares")
+
+let square
+let num
+numSq.addEventListener("keydown",(event)=>{
+
+    if (event.key=="Enter"){
+        
+        num=numSq.value
+
+        for (let i=0;i<(num**2);i++){
+        let a=document.createElement("div")
+        a.style.flex=`1 1 ${100/num}%`
+        a.style.height=`${100/num}%`
+        a.className="b"
+        main.appendChild(a)
+        }
+    }
 })
+
+
+let b=document.querySelectorAll(".b")
+main.addEventListener("mouseover",(event)=>{
+    if (event.target!=main){
+        event.target.style.backgroundColor=`black`
+    }
+})
+let reset=document.querySelector(".reset")
+reset.addEventListener("click",()=>{
+    let square
+})
+//event.target.style.backgroundColor=`#${(Math.floor(Math.random()*1000000)).toString().padStart(6,"0")}`
+//for rainbow mode
