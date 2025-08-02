b9deb25b6ab5479f63d660f6939e34d0d9195927
alfin-cs <alfinsathesh@gmail.com>
2025-08-03 00:57:32 +0300

    only need rainbow!!!!! and optacity

--- change.js
+++ change.js
@@ -4,24 +4,30 @@ let numSq=document.querySelector("#squares")
 
 let square
 let num
-numSq.addEventListener("keydown",(event)=>{
+numSq.addEventListener("keydown",(event)=> {
 
     if (event.key=="Enter"){
+        createDiv()
+    }
+})
+function createDiv(){
+    main.innerHTML=""
         
-        num=numSq.value
+    num=numSq.value
+    
 
-        for (let i=0;i<(num**2);i++){
+    for (let i=0;i<(num**2);i++){
         let a=document.createElement("div")
         a.style.flex=`1 1 ${100/num}%`
         a.style.height=`${100/num}%`
         a.className="b"
         main.appendChild(a)
-        }
     }
-})
+}
 
 
 let b=document.querySelectorAll(".b")
+console.log(b)
 main.addEventListener("mouseover",(event)=>{
     if (event.target!=main){
         event.target.style.backgroundColor=`black`
@@ -29,7 +35,9 @@ main.addEventListener("mouseover",(event)=>{
 })
 let reset=document.querySelector(".reset")
 reset.addEventListener("click",()=>{
-    let square
+     main.innerHTML=""
+     createDiv()
 })
+
 //event.target.style.backgroundColor=`#${(Math.floor(Math.random()*1000000)).toString().padStart(6,"0")}`
 //for rainbow mode
